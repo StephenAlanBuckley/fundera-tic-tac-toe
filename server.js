96cfc1720b56d592f582733cf32638dc68ef4b83
Stephen Buckley <stephen.alan.buckley@gmail.com>
2018-03-21 12:28:52 -0400

    starting frontend

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ const port = process.env.PORT || 5000
 
 const app = express();
 
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+//keep our static files (client js and css plus any images) in a public folder
+app.use(express.static(__dirname + '/public'))
+
 app.get("/", (req, res) => {
-  res.send("Tic-Tac-Toe " + Date.now());
+  res.render('index',
+      {title: 'Tic-Tac-Toe'}
+  )
 });
 
 app.listen(port, () => {
